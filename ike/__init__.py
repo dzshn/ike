e392260bc7a823e8beea16ea35daaaf2f7253a7b
Sofia N. Lima <me@dzshn.xyz>
2022-07-18 19:16:43 -0300

    feat: `dump_pyobj`

--- ike/__init__.py
+++ ike/__init__.py
@@ -248,3 +248,22 @@ def byc(func: type(byc)) -> type(byc):
         freevars,
     )
     return func
+
+
+def dump_pyobj(thing: object) -> bytes:
+    """Read a ``PyObject``'s raw bytes.
+
+    Parameters
+    ----------
+    thing : object
+        Literally anything.
+
+    Returns
+    -------
+    bytes
+    """
+    x = id(thing)
+    return bytes(
+        ctypes.c_ubyte.from_address(x + i).value
+        for i in range(thing.__sizeof__())
+    )
