80b730ec479c266c7492fa5a4e7973e5d0111c0a
Sofia N. Lima <me@dzshn.xyz>
2022-07-23 05:00:44 -0300

    feat: genericize memory functions

--- ike/__init__.py
+++ ike/__init__.py
@@ -1,4 +1,14 @@
 from ike.bytecode import byc, code
-from ike.memory import dump_pyobj
+from ike.memory import peek, peeko, poke, sizeof, view, viewo
 
-__all__ = ("byc", "code", "dump_pyobj")
+__all__ = (
+    "byc",
+    "code",
+    "peek",
+    "peeko",
+    "poke",
+    "sizeof",
+    "view",
+    "view",
+    "viewo",
+)
--- ike/memory.py
+++ ike/memory.py
@@ -1,20 +1,36 @@
-import ctypes
+"""Functions for peeking and poking memory."""
+from ctypes import c_int, c_ssize_t, c_ubyte, c_void_p, py_object, pythonapi
 
 
-def dump_pyobj(thing: object) -> bytes:
-    """Read a ``PyObject``'s raw bytes.
+def sizeof(thing: object) -> int:
+    """Get the size of an object in memory."""
+    for cls in thing.__class__.__mro__:
+        if cls.__sizeof__.__class__ is not (lambda: 0 / 0).__class__:
+            return cls.__sizeof__(thing)
 
-    Parameters
-    ----------
-    thing : object
-        Literally anything.
 
-    Returns
-    -------
-    bytes
-    """
-    x = id(thing)
-    return bytes(
-        ctypes.c_ubyte.from_address(x + i).value
-        for i in range(thing.__sizeof__())
-    )
+def peek(addr: int, length: int = 1) -> bytes:
+    """Read raw bytes from memory."""
+    return bytes((c_ubyte * length).from_address(addr))
+
+
+def poke(addr: int, data: bytes):
+    """Write raw bytes into memory."""
+    (c_ubyte * len(data)).from_address(addr)[:] = data
+
+
+def view(addr: int, length: int) -> memoryview:
+    """Return a memoryview pointed to arbitrary memory."""
+    fn = pythonapi.PyMemoryView_FromMemory
+    fn.restype = py_object
+    return fn(c_void_p(addr), c_ssize_t(length), c_int(0x200))
+
+
+def peeko(thing: object) -> bytes:
+    """``peek`` an entire object."""
+    return peek(id(thing), sizeof(thing))
+
+
+def viewo(thing: object) -> memoryview:
+    """``view`` an entire object."""
+    return view(id(thing), sizeof(thing))
